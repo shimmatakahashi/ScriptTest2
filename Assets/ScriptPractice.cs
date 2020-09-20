f816b1f32918b4810a9dd8da16eb80be558f36cf
shimmatakahashi <70498393+shimmatakahashi@users.noreply.github.com>
2020-09-20 09:27:59 +0900

    Initial commit

---
+++ Assets/ScriptPractice.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptPractice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] cnt = { 1, 2, 3, 4, 5 };
+        for(int i = 0; i <= 4; i++)
+        {
+            Debug.Log(cnt[i]);
+        }
+        for(int i = 4; i >= 0; i--)
+        {
+            Debug.Log(cnt[i]);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
